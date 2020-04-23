b9f82d599d5b24dce4f460cf58369685a9a245cc
Victor Sotskov <51897005+VicRul@users.noreply.github.com>
2020-04-23 12:15:55 +0700

    123

--- src/models/BaseModel.java
+++ src/models/BaseModel.java
@@ -9,6 +9,8 @@ import java.util.HashMap;
 public class BaseModel {
 	 
 	// 123
+	//123456
+	//12345678
 	static ArrayList<Goods> goods = new ArrayList<Goods>();
 	static ArrayList<Basket> basket = new ArrayList<Basket>();
 	static ArrayList<Users> users = new ArrayList<Users>();
