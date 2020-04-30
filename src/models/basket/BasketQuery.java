4345936b7c8254dc4b5e62a5e08e449bee129977
Victor Sotskov <51897005+VicRul@users.noreply.github.com>
2020-04-30 23:35:08 +0700

    Добавил поиск заказов по статусу, перегрузил getAllUserOrders

--- src/models/basket/BasketQuery.java
+++ src/models/basket/BasketQuery.java
@@ -12,62 +12,37 @@ public class BasketQuery {
 
 	private static ArrayList<Basket> basket = new ArrayList<Basket>();
 
-	/* Открываем корзину для авторизованного пользователя */
-	public static int openBasket(int idUser)
-			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
-			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
-
-		/*int idBasket = 0;
-		ResultSet rs = ORM.select("all_baskets", new String[] { "id_basket" }, "where id_user = '" + idUser + "'");
-
-		if (rs.next()) {
-			idBasket = rs.getInt("id_basket");
-			rs.close();
-			return idBasket;
-		}
-
-		rs.close();*/
-		
-		
-		int idBasket = ORM.findMaxId("id_basket", "all_baskets") + 1;
-		HashMap<String, String> values = new HashMap<String, String>();
-		values.put("id_basket", Integer.toString(idBasket));
-		values.put("id_user", Integer.toString(idUser));
-		ORM.insert("all_baskets", values);
-		return idBasket;
-	}
-
 	/* Добавить товар в корзину */
-	public static boolean addGoodsToBasket(int idGood, int idBasket)
+	public static boolean addGoodsToBasket(int idGood, int idOrder)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
 			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
 
 		ResultSet rs = ORM.select("basket", new String[] { "id_good", "count" },
-				"where id_good = " + idGood + " and id_basket = " + idBasket);
+				"where id_good = " + idGood + " and id_order = " + idOrder);
 		HashMap<String, String> values = new HashMap<String, String>();
 
 		if (rs.next()) {
 			int c = rs.getInt("count");
 			values.put("count", Integer.toString(++c));
 			rs.close();
-			return ORM.update("basket", values, "where id_good = " + idGood + " and id_basket = " + idBasket);
+			return ORM.update("basket", values, "where id_good = " + idGood + " and id_order = " + idOrder);
 		}
 		int id = ORM.findMaxId("id_purchase", "basket") + 1;
 		values.put("id_purchase", Integer.toString(id));
 		values.put("id_good", Integer.toString(idGood));
 		values.put("count", "1");
-		values.put("id_basket", Integer.toString(idBasket));
+		values.put("id_order", Integer.toString(idOrder));
 		rs.close();
 		return ORM.insert("basket", values);
 	}
 
 	/* Удалить товар из корзины */
-	public static boolean removeGoodsFromBasket(int idGood, int idBasket)
+	public static boolean removeGoodsFromBasket(int idGood, int idOrder)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
 			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
 
 		ResultSet rs = ORM.select("basket", new String[] { "id_good", "count" },
-				"where id_good = " + idGood + " and id_basket = " + idBasket);
+				"where id_good = " + idGood + " and id_order = " + idOrder);
 		HashMap<String, String> values = new HashMap<String, String>();
 
 		while (rs.next()) {
@@ -75,20 +50,20 @@ public class BasketQuery {
 			if (c > 1) {
 				values.put("count", Integer.toString(--c));
 				rs.close();
-				return ORM.update("basket", values, "where id_good = " + idGood + " and id_basket = " + idBasket);
+				return ORM.update("basket", values, "where id_good = " + idGood + " and id_order = " + idOrder);
 			}
 		}
-		return ORM.delete("basket", "where id_good = '" + idGood + "' and id_basket = '" + idBasket + "'");
+		return ORM.delete("basket", "where id_good = '" + idGood + "' and id_order = '" + idOrder + "'");
 	}
 
 	/* Получить список товаров в корзине */
-	public static ArrayList<Basket> getGoodsFromBasket(int idBasket)
+	public static ArrayList<Basket> getGoodsFromBasket(int idOrder)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
 			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
 
 		String[] fields = new String[] { "goods.id_good", "img", "title", "price", "img", "count" };
 		ResultSet rs = ORM.select("goods inner join basket on goods.id_good=basket.id_good", fields,
-				" where id_basket = " + idBasket);
+				" where id_order = " + idOrder);
 		basket.clear();
 		while (rs.next()) {
 			String img = "img\\" + rs.getString("img");
@@ -100,5 +75,14 @@ public class BasketQuery {
 		}
 		rs.close();
 		return basket;
+	}
+	
+	public static void main(String[] args)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
+		
+		addGoodsToBasket(4, 3);
+		
+		
 	}
 }
--- src/models/orders/OrdersQuery.java
+++ src/models/orders/OrdersQuery.java
@@ -33,56 +33,86 @@ public class OrdersQuery {
 	}
 
 	/* Оформляем заказ */
-	public static int createOrder(int idBasket)
+	public static int createOrder(int idUser)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
 			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
 		int idOrder = ORM.findMaxId("id_order", "orders") + 1;
-		
+
 		HashMap<String, String> values = new HashMap<String, String>();
 		values.put("id_order", Integer.toString(idOrder));
-		values.put("id_basket", Integer.toString(idBasket));
-		ORM.insert("orders", values);		
-		
+		values.put("id_user", Integer.toString(idUser));
+		ORM.insert("orders", values);
+
 		return idOrder;
 	}
 
-	/* Получаем все заказы конкретного пользователя !!! НЕ РАБОТАЕТ !!!*/
+	/* Подтверждение заказа */
+	public static boolean submitOrder(int idOrder)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
+
+		ResultSet rs = ORM.select("basket b inner join goods g on g.id_good = b.id_good",
+				new String[] { "SUM(price * b.count)" }, "where id_order = " + idOrder);
+		int sumOrder = 0;
+
+		if (rs.next()) {
+			sumOrder = rs.getInt(1);
+		}
+
+		HashMap<String, String> values = new HashMap<String, String>();
+		values.put("sum_order", Integer.toString(sumOrder));
+		values.put("id_status", "2");
+
+		rs.close();
+		return ORM.update("orders", values, "where id_order = " + idOrder);
+	}
+
+	/* Получаем все заказы конкретного пользователя */
 	public static ArrayList<Orders> getAllUserOrders(int idUser)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
 			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
-		ResultSet rs = ORM.select("status s inner join orders o on s.id_status = o.id_status inner join all_baskets a on a.id_basket = o.id_basket",
-				new String[] {"id_order", "status"}, "where a.id_user = " + idUser);
+
+		ResultSet rs = ORM.select("orders o inner join status s on s.id_status = o.id_status",
+				new String[] { "id_order", "status", "sum_order" }, "where id_user = " + idUser);
 		orders.clear();
 		while (rs.next()) {
 			int idOrder = rs.getInt("id_order");
-			ResultSet sumRs = ORM.select("goods g inner join basket b on g.id_good = b.id_good inner join all_baskets a on a.id_basket = a.id_basket", 
-					new String[] {"SUM(price * b.count) as sum"}, "where a.id_user = " + idUser);
-			int sumOrder = 0;
-			if (sumRs.next()) {
-				sumOrder = sumRs.getInt("sum");
-			}
+			int sumOrder = rs.getInt("sum_order");
 			String status = rs.getString("status");
-			sumRs.close();
+
 			orders.add(new Orders(idOrder, sumOrder, status));
 		}
-		
+
 		return orders;
 	}
-	
+
 	/* Получаем все заказы конкретного пользователя в зависимости от статуса */
 	public static ArrayList<Orders> getAllUserOrders(int idUser, int idStatus)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
 			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
-		
-		return orders;	
+
+		ResultSet rs = ORM.select("orders o inner join status s on s.id_status = o.id_status",
+				new String[] { "id_order", "status", "sum_order" }, "where id_user = " + idUser + " and o.id_status = " + idStatus);
+		orders.clear();
+		while (rs.next()) {
+			int idOrder = rs.getInt("id_order");
+			int sumOrder = rs.getInt("sum_order");
+			String status = rs.getString("status");
+
+			orders.add(new Orders(idOrder, sumOrder, status));
+		}
+
+		return orders;
 	}
-	
+
 	public static void main(String[] args)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
 			InvocationTargetException, NoSuchMethodException, SecurityException, SQLException {
+
 		
-		ArrayList<Orders> values = getAllUserOrders(1);
 		
+		ArrayList<Orders> values = getAllUserOrders(1, 2);
+
 		for (Orders value : values) {
 			System.out.println(value);
 		}
